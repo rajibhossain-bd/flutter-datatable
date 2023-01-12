95ffc1b369fb9d0857a608fcd4dd43776bd513d2
rajibhossain <rajibhossain@cayto.jp>
2023-01-12 17:08:43 +0600

    structuring the repo!

--- lib/article/repository/repository_provider.dart
+++ lib/article/repository/repository_provider.dart
@@ -1,7 +1,8 @@
 //import 'package:flutter_datatable/article/repository/article/article_repository.dart';
-import 'package:flutter_datatable/article/repository/article_repository_demo_impl.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:flutter_datatable/article/repository/article_repository.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final articleRepositoryProvider = Provider<ArticleRepository>(
   (ref) => ArticleRepository(),
+  //ArticleRepository.new,
 );
